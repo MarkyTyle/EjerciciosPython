f0893065835c3c8f1257d090928c1c80c894d9f0
Marcos.Delgado <mdelgado@enatrel.gob.ni>
2025-09-01 13:25:14 -0600

    3 commit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import requests
 app = FastAPI(
     title="Fast APi Python V1.2.35",
     description="API Sencilla en Python con FastAPI",
@@ -8,3 +9,17 @@ app = FastAPI(
 @app.get("/")
 def read_root():
     return {"Bienvenido a mi API"}
+
+@app.get("/chiste")
+def obtener_chiste():
+    respuesta = requests.get("https://official-joke-api.appspot.com/random_joke")
+    if not respuesta.ok:
+        print("Ocurrio un error en la soliictud Api")
+    else:
+        data = respuesta.json()
+        return {
+        "categoria" : data.get("type"),
+        "pregunta" : data.get("setup"),
+        "respuesta" : data.get("punchline")
+        }
+
